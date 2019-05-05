2037543cf0bf7d294f536c08b98205de5f1307bf
djoci <zorica_mitrovic@mail.com>
2019-05-05 17:09:01 +0200

    Test checkdate

--- src/js/math.js
+++ src/js/math.js
@@ -10,5 +10,39 @@ const proveriIme = function () {
  }
  }
 
- module.exports = proveriIme
+  function checkdate(exdate) {
+       exdate = exdate.split('/');
+       error=0;
+       // Proveravamo duzinu
+       if (exdate.length == 2) {
+          let month=exdate[0];
+          let year=exdate[1];
+          month = parseInt(month);	        
+          year = parseInt(year);
+          // proveravamo mesec   
+          if (month < 1 || month >12 || isNaN(month)) {
+              error = 1;	        	
+          }
+          // proveravamo godinu  
+          if (year < new Date().getFullYear() || isNaN(year) ) {
+                error = 1;
+          }
+         // provera ako istice ove godine  
+         if (year == new Date().getFullYear() && month <= new Date().getMonth()) {
+                error = 1;
+          }
+       }
+       else {
+       // pogresan format -> greska
+           error = 1;
+       }
+        
+       if (error==1) {
+        return false;      	
+       } else {
+        return true;   
+       }        
+  }
+
+ module.exports = checkdate
 
--- src/js/math.test.js
+++ src/js/math.test.js
@@ -1,15 +1,21 @@
-const proveriIme = require('./math')
-describe('proveriIme', () => {
-  it('ime should match with /^[A-ZČĆŠĐŽ]{1}[a-zčćšđž]{1,12}$/', () => {
+const checkdate = require('./math')
+describe('checkdate', () => {
+  it('Datum mesec i godina mogu da budu tekuci ili u buducnosti', () => {
     expect(true).toBeTruthy();
   });
 
-  test('/^[A-ZČĆŠĐŽ]{1}[a-zčćšđž]{1,12}$/.test(ime)', () => {
-    expect('Anamarija').toMatch(/^[A-ZČĆŠĐŽ]{1}[a-zčćšđž]{1,12}$/);
-  });
+test('Buduci datum', () => {
+  expect(checkdate('8/2019')).toBe(true);
+});
 
-  test('/^[A-ZČĆŠĐŽ]{1}[a-zčćšđž]{1,12}$/.test(ime)', () => {
-    expect('1234').toMatch(/^[A-ZČĆŠĐŽ]{1}[a-zčćšđž]{1,12}$/);
-  });
+test('Datum u proslosti', () => {
+  expect(checkdate('8/2011')).toBe(false);
+});
+
+test('Pogresan unos', () => {
+  expect(checkdate('18/2011')).toBe(false);
+});
+
 
 });
+
